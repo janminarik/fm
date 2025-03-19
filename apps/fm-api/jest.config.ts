c3bb202fce7e7ea9fe785a2699c63a0db0238730
Jan Minarik <jan@janminarik.sk>
2025-03-19 15:17:58 +0100

    test cov

--- apps/fm-api/jest.config.ts
+++ apps/fm-api/jest.config.ts
@@ -2,13 +2,18 @@ import { type Config } from "@jest/types";
 
 const config: Config.InitialOptions = {
   displayName: "fm-api",
+
   preset: "ts-jest",
   testEnvironment: "node",
+
   rootDir: ".",
   roots: ["<rootDir>/src", "<rootDir>/e2e"],
   testRegex: ".*\\.spec\\.ts$|.*\\.e2e.spec\\.ts$",
-  collectCoverageFrom: ["**/*.(t|j)s"],
+
   coverageDirectory: "../../coverage/app/fm-api",
+
+  collectCoverage: true,
+  collectCoverageFrom: ["**/*.{ts,tsx}"],
 };
 
 export default config;
