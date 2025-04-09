c61ec8d928c6937e6cb92163a338e750076ab022
Jan Minarik <jan@janminarik.sk>
2025-04-09 07:04:52 +0200

    refactor: exclude additional test files from coverage collection in jest configuration

--- apps/fm-api/jest.config.mjs
+++ apps/fm-api/jest.config.mjs
@@ -8,11 +8,13 @@ const config = {
   testRegex: ".*\\.spec\\.ts$|.*\\.test\\.ts$",
   coverageDirectory: "../../coverage/app/fm-api",
   collectCoverageFrom: [
-    "<rootDir>/src/**/*.ts", 
-    "!<rootDir>/src/**/*.spec.ts", 
-    "!<rootDir>/src/**/*.test.ts", 
-    "!<rootDir>/src/**/*.module.ts", 
-    "!<rootDir>/src/main.ts"
+    "<rootDir>/src/**/*.ts",
+    "!<rootDir>/src/**/*.spec.ts",
+    "!<rootDir>/src/**/*.test.ts",
+    "!<rootDir>/src/**/*.module.ts",
+    "!<rootDir>/src/main.ts",
+    "!<rootDir>/src/utils/test/*.ts",
+    "!<rootDir>/swagger.ts",
   ],
   transform: {
     "^.+\\.ts?$": [
