0b06447a6a251d403ff37c09b33990a3f1a00ee4
Jan Minarik <jan@janminarik.sk>
2025-04-11 09:03:36 +0200

    refactor: remove unused UserController file

--- apps/fm-api/src/api/user/user.controller.ts
+++
@@ -1,8 +0,0 @@
-import { Controller } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
-
-@ApiTags("user")
-@Controller({ version: "1", path: "user" })
-export class UserController {
-  //TODO: delete, updatePhone, updateUserName, updateEmail
-}
