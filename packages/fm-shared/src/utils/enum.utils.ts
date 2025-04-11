39d2645979e3157355ffe633a98c6e6b58ef9aa0
Jan Minarik <jan@janminarik.sk>
2025-04-11 17:15:01 +0200

    refactor: fix parameter formatting in mapEnumValue function for consistency

--- packages/fm-shared/src/utils/enum.utils.ts
+++ packages/fm-shared/src/utils/enum.utils.ts
@@ -2,7 +2,7 @@ export type EnumLike = { [key: string]: string | number };
 
 export function mapEnumValue<TargetEnum extends EnumLike>(
   value: unknown,
-  targetEnum: TargetEnum
+  targetEnum: TargetEnum,
 ) {
   return targetEnum[value as keyof TargetEnum];
 }
