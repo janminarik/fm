8062775e26d9e6bf6e6e3a5389af23fdec9bdf17
Jan Minarik <jan@janminarik.sk>
2025-04-11 10:23:07 +0200

    refactor: update AuthService tests for improved mock usage and consistency

--- apps/fm-api/src/api/auth/services/auth.service.spec.ts
+++ apps/fm-api/src/api/auth/services/auth.service.spec.ts
@@ -1,4 +1,11 @@
-import { jest, beforeAll, describe, expect, beforeEach } from "@jest/globals";
+import {
+  jest,
+  describe,
+  expect,
+  beforeEach,
+  test,
+  afterEach,
+} from "@jest/globals";
 import { UnauthorizedException } from "@nestjs/common";
 import { Test } from "@nestjs/testing";
 import {
@@ -11,7 +18,7 @@ import { AuthToken } from "@repo/fm-auth";
 import { type IUserRepository, User, USER_REPOSITORY } from "@repo/fm-domain";
 import { createUserFake } from "@repo/fm-mock-data";
 import { HASH_SERVICE, type IHashService } from "@repo/fm-shared";
-import { mock } from "jest-mock-extended";
+import { mock, MockProxy } from "jest-mock-extended";
 import { ClsModule } from "nestjs-cls";
 
 import { AuthService } from "../services/auth.service";
@@ -24,16 +31,16 @@ jest.mock("@nestjs-cls/transactional", () => ({
 
 describe("AuthService", () => {
   let authService: AuthService;
-  let userRepositoryMock: jest.Mocked<IUserRepository>;
-  let hashServiceMock: jest.Mocked<IHashService>;
-  let accessTokenServiceMock: jest.Mocked<IAccessTokenService>;
-  let refreshTokenServiceMock: jest.Mocked<IRefreshTokenService>;
+  let mockUserRepository: MockProxy<IUserRepository>;
+  let mockHashService: MockProxy<IHashService>;
+  let mockAccessTokenService: MockProxy<IAccessTokenService>;
+  let mockRefreshTokenService: MockProxy<IRefreshTokenService>;
 
-  beforeAll(async () => {
-    userRepositoryMock = mock<IUserRepository>();
-    hashServiceMock = mock<IHashService>();
-    accessTokenServiceMock = mock<IAccessTokenService>();
-    refreshTokenServiceMock = mock<IRefreshTokenService>();
+  beforeEach(async () => {
+    mockUserRepository = mock<IUserRepository>();
+    mockHashService = mock<IHashService>();
+    mockAccessTokenService = mock<IAccessTokenService>();
+    mockRefreshTokenService = mock<IRefreshTokenService>();
 
     const moduleFixture = await Test.createTestingModule({
       imports: [
@@ -45,19 +52,19 @@ describe("AuthService", () => {
       providers: [
         {
           provide: USER_REPOSITORY,
-          useValue: userRepositoryMock,
+          useValue: mockUserRepository,
         },
         {
           provide: HASH_SERVICE,
-          useValue: hashServiceMock,
+          useValue: mockHashService,
         },
         {
           provide: ACCESS_TOKEN_SERVICE,
-          useValue: accessTokenServiceMock,
+          useValue: mockAccessTokenService,
         },
         {
           provide: REFRESH_TOKEN_SERVICE,
-          useValue: refreshTokenServiceMock,
+          useValue: mockRefreshTokenService,
         },
         AuthService,
       ],
@@ -66,9 +73,8 @@ describe("AuthService", () => {
     authService = moduleFixture.get<AuthService>(AuthService);
   });
 
-  beforeEach(() => {
+  afterEach(() => {
     jest.clearAllMocks();
-    jest.restoreAllMocks();
   });
 
   test("should init", () => {
@@ -77,51 +83,51 @@ describe("AuthService", () => {
 
   describe("login", () => {
     test("should login", async () => {
-      const userMock: User = createUserFake();
-      const email = userMock.email;
+      const mockUser: User = createUserFake();
+      const email = mockUser.email;
       const password = "password-hash";
 
-      const accessTokenMock: AuthToken = {
+      const mockAccessToken: AuthToken = {
         token: "token-value",
         expiresAt: 123,
       };
 
-      const refreshTokenMock: AuthToken = {
+      const mockRefreshToken: AuthToken = {
         token: "token-value",
         expiresAt: 123,
       };
 
-      userRepositoryMock.findUserByEmail.mockResolvedValue(userMock);
-      accessTokenServiceMock.createToken.mockResolvedValue(accessTokenMock);
-      refreshTokenServiceMock.createToken.mockResolvedValue(refreshTokenMock);
-      hashServiceMock.compare.mockResolvedValue(true);
+      mockUserRepository.findUserByEmail.mockResolvedValue(mockUser);
+      mockAccessTokenService.createToken.mockResolvedValue(mockAccessToken);
+      mockRefreshTokenService.createToken.mockResolvedValue(mockRefreshToken);
+      mockHashService.compare.mockResolvedValue(true);
 
       const result = await authService.login(email, password);
 
-      expect(userRepositoryMock.findUserByEmail).toHaveBeenCalledWith(email);
-      expect(hashServiceMock.compare).toHaveBeenCalled();
-      expect(accessTokenServiceMock.createToken).toHaveBeenCalledWith(
-        userMock.id,
+      expect(mockUserRepository.findUserByEmail).toHaveBeenCalledWith(email);
+      expect(mockHashService.compare).toHaveBeenCalled();
+      expect(mockAccessTokenService.createToken).toHaveBeenCalledWith(
+        mockUser.id,
       );
-      expect(refreshTokenServiceMock.createToken).toHaveBeenCalledWith(
-        userMock.id,
+      expect(mockRefreshTokenService.createToken).toHaveBeenCalledWith(
+        mockUser.id,
       );
 
       expect(result).toEqual({
-        accessToken: accessTokenMock.token,
-        accessTokenExpiresAt: accessTokenMock.expiresAt,
-        refreshToken: refreshTokenMock.token,
-        refreshTokenExpiresAt: refreshTokenMock.expiresAt,
+        accessToken: mockAccessToken.token,
+        accessTokenExpiresAt: mockAccessToken.expiresAt,
+        refreshToken: mockRefreshToken.token,
+        refreshTokenExpiresAt: mockRefreshToken.expiresAt,
       });
     });
 
     test("login should fail when user does not exist", async () => {
-      const userMock: User = createUserFake();
-      const email = userMock.email;
+      const mockUser: User = createUserFake();
+      const email = mockUser.email;
       const password = "password-hash";
       const expectedError = new UnauthorizedException("Invalid credentials");
 
-      userRepositoryMock.findUserByEmail.mockResolvedValue(null);
+      mockUserRepository.findUserByEmail.mockResolvedValue(null);
 
       await expect(authService.login(email, password)).rejects.toThrow(
         expectedError,
@@ -129,14 +135,14 @@ describe("AuthService", () => {
     });
 
     test("login should fail when user is not verified", async () => {
-      const userMock: User = createUserFake();
-      const email = userMock.email;
+      const mockUser: User = createUserFake();
+      const email = mockUser.email;
       const password = "password-hash";
       const expectedError = new UnauthorizedException("Invalid credentials");
 
-      userRepositoryMock.findUserByEmail.mockResolvedValue(userMock);
+      mockUserRepository.findUserByEmail.mockResolvedValue(mockUser);
 
-      hashServiceMock.compare.mockResolvedValue(false);
+      mockHashService.compare.mockResolvedValue(false);
 
       await expect(authService.login(email, password)).rejects.toThrow(
         expectedError,
@@ -149,7 +155,7 @@ describe("AuthService", () => {
       const password = "password";
       const passwordHash = "password-hash";
 
-      hashServiceMock.compare.mockResolvedValue(true);
+      mockHashService.compare.mockResolvedValue(true);
 
       const result = await authService.verifyUserPassword(
         password,
